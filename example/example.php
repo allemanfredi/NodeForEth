9c538bbfbdb16507f73c7611d4af6438b2fef946
Alessandro Manfredi <alle.manfredi@gmail.com>
2018-04-03 17:52:02 +0200

    Update example.php

--- example/example.php
+++ example/example.php
@@ -1,16 +1,27 @@
+
 <?php
-
 include_once dirname(__FILE__) . '/NodeForEth.php';
+$debug = false;
+$NodeForEth = new NodeForEth($debug , 'Ktmsxf2502015' ,'https://rinkeby.infura.io/QQghNfFwGsfvd2rA8mjp' , '/usr/local/bin/node' );
+$result = $NodeForEth->getBalance('0x9596f1439576e0142976F0822883DE62e326B542');
 
-$debug = true;
-$NodeForEth = new NodeForEth($debug , 'your password' ,'https://rinkeby.infura.io/QQghNfFwGsfvd2rA8mjp' , '/usr/local/bin/node' );
+if (  trim($result[1] ) === "error" ){
+  echo $result[2];
+}else{
+  $balance = $result[1];
+  echo $balance;
+}
 
-
-echo $NodeForEth->getBalance('0x281055afc982d96fab65b3a49cac8b878184cb16');
-
-$NodeForEth->sendTx('0x9596f1439576e0142976F0822883DE62e326B542' ,
+$result = $NodeForEth->sendTx('0x9596f1439576e0142976F0822883DE62e326B542' ,
                      '983ab3335943f82ecb78e941ad03ddd60e149d8a02be8ca4c0c2a65bf48292cc',
-                     '0xeF19E7Ec9eE90a0426c60aFCc504C02513E11' ,
+                     '0xbd95911be6E070e23569bcbF612aEad91afC0002' ,
                      0.010000 );
-                    
+ if (  trim($result[1] ) === "error" ){
+   echo $result[2];
+ }
+ else{
+   $transaction_id = $result[1];
+   echo $transaction_id;
+ }
+
 ?>
