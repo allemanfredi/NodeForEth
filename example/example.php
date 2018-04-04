bd93a55519afe0aa9f6d58310cd7911d91b1277c
Alessandro Manfredi <alle.manfredi@gmail.com>
2018-04-04 16:05:59 +0200

    Update example.php

--- example/example.php
+++ example/example.php
@@ -25,4 +25,18 @@
      echo $transaction_id;
    }
 
+
+  $result = $NodeForEth->sendERC20('0x9596f1439576e0142976F0822883DE62e326B542' ,
+                      '983ab3335943f82ecb78e941ad03ddd60e149d8a02be8ca4c0c2a65bf48292cc',
+                      '0x5eE8f0ec28F6Fae0c79D5dd37DbB66256b2911f4' ,
+                      '0x4C20aEa100612472BB598A789c6b1D43B88cee55' ,
+                       20000 );
+  if (  trim($result[1] ) === "error" ){
+    echo $result[2];
+  }
+  else{
+    $transaction_id = $result[1];
+    echo $transaction_id;
+  }
+
 ?>
